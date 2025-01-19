18fd73a00a739f7ba234321d413730045e3f3478
SameeraDronavalli <sameeradronavalli327@gmail.com>
2025-01-19 20:55:48 +0530

    Add files via upload

---
+++ practice1.js
@@ -0,0 +1,11 @@
+let myName = "Sameera";
+let lastName = "Dronavalli";
+let personAge = 24;
+let isStudent = "false" ;
+const graduationYear = 2021;
+// graduationYear = 2023; // const shows error because after declaring a variable we cannot change the value 
+console.log("First Name:", myName);
+console.log("Last Name:", lastName);
+console.log("Age:", personAge);
+console.log("Status:", isStudent);
+console.log("Graduation Year:", graduationYear);
